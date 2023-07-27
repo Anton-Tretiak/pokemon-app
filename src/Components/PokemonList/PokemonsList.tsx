78b631c8c46759b8d11eb4bee1badabf16a25f82
Anton-Tretiak <tretiak.ap@gmail.com>
2023-07-27 19:58:14 +0300

    Create functionality for buttons 'Load More' and 'Go Back'. Create Loader for List.

--- src/Components/PokemonList/PokemonsList.tsx
+++ src/Components/PokemonList/PokemonsList.tsx
@@ -3,16 +3,17 @@ import './PokemonList.scss';
 import { fetchPokemonAPI } from '../../API/PokeAPI';
 
 import { PokemonsResponse } from '../../Types/PokemonsResponse';
-import { Pokemon } from '../../Types/Pokemon';
 import { PokemonDetails } from '../../Types/PokemonDetails';
 
 import { PokemonItem } from '../PokemonItem/PokemonItem';
+import { Loader } from '../Loader/Loader';
 
 export const PokemonsList = () => {
   const [pokemonsData, setPokemonsData] = useState<PokemonsResponse | null>(
     null,
   );
   const [pokemonDetails, setPokemonDetails] = useState<PokemonDetails[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getPokemonsData = async() => {
     try {
@@ -23,47 +24,96 @@ export const PokemonsList = () => {
       throw new Error('Error while fetching data');
     }
   };
+  
+  const fetchPokemonDetails = async(pokemonUrls: string[]) => {
+    try {
+      const promises = pokemonUrls.map(async(url: string) => {
+        const response = await fetch(url);
+        
+        return response.json();
+      });
+      
+      const pokemonDetailsArray = await Promise.all(promises);
+      
+      setPokemonDetails(pokemonDetailsArray);
+    } catch {
+      throw new Error('Error while fetching pokemon details');
+    }
+  };
 
   useEffect(() => {
     getPokemonsData();
   }, []);
 
   useEffect(() => {
-    const fetchPokemonDetails = async() => {
-      if (pokemonsData) {
-        const promises = pokemonsData.results.map(async(pokemon: Pokemon) => {
-          const response = await fetch(pokemon.url);
-
-          return response.json();
-        });
-
-        const pokemonDetailsArray = await Promise.all(promises);
-
-        setPokemonDetails(pokemonDetailsArray);
-      }
-    };
-
-    fetchPokemonDetails();
+    if (pokemonsData) {
+      const pokemonUrls = pokemonsData.results.map((pokemon) => pokemon.url);
+      
+      fetchPokemonDetails(pokemonUrls);
+    }
   }, [pokemonsData]);
+  
+  const fetchPage = async(url: string) => {
+    try {
+      setIsLoading(true);
+      
+      const response = await fetch(url);
+      const data: PokemonsResponse = await response.json();
+      
+      setPokemonsData(data);
+      
+      const pokemonUrls = data.results.map((pokemon) => pokemon.url);
+      
+      fetchPokemonDetails(pokemonUrls);
+    } catch {
+      throw new Error('Error while fetching page');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  
+  const loadNextPage = async() => {
+    if (pokemonsData?.next) {
+      fetchPage(pokemonsData.next);
+    }
+  };
+  
+  const loadPreviousPage = async() => {
+    if (pokemonsData?.previous) {
+      fetchPage(pokemonsData.previous);
+    }
+  };
 
   return (
     <div className='list-container'>
-      <div className='list box'>
-        {pokemonDetails.length && (
-          pokemonDetails.map((pokemon: PokemonDetails, index: number) => (
-            <PokemonItem
-              key={index}
-              pokemon={pokemon}
-            />
-          ))
+      <div className={`list box ${isLoading && 'loading-height'}`}>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          pokemonDetails.length && (
+            pokemonDetails.map((pokemon: PokemonDetails, index: number) => (
+              <PokemonItem
+                key={index}
+                pokemon={pokemon}
+              />
+            ))
+          )
         )}
         
         <div className='list__buttons'>
-          <button className='button is-info list__button'>
+          <button
+            className='button is-info list__button'
+            onClick={loadPreviousPage}
+            disabled={!pokemonsData?.previous}
+          >
             Go Back
           </button>
           
-          <button className='button is-info list__button'>
+          <button
+            className='button is-info list__button'
+            onClick={loadNextPage}
+            disabled={!pokemonsData?.next}
+          >
             Load More
           </button>
         </div>
