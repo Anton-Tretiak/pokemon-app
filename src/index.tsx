4d48a73aecb065b04a98e6c129a60a54dd52441b
Anton-Tretiak <tretiak.ap@gmail.com>
2023-07-29 14:39:36 +0300

    Refactor and optimize all code.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import 'bulma/css/bulma.css';
 import './index.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(
